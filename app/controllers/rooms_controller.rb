e46bdd711b82f5edd1b6e71e623e9145e0ca84d7
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-29 16:49:04 +0545

    Add CRUD functionality in controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,50 @@
 class RoomsController < ApplicationController
-  def edit
-  end
+  before_action :find_room, only: [:show, :edit, :update, :destroy]
 
   def index
   end
 
-  def new
+  def show
   end
 
-  def show
+  def new
+    @room = Room.new
+  end
+
+  def edit
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      redirect_to @room
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to @room
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @room.destroy
+
+    redirect_to rooms_path
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :floor_number, :capacity, :room_image)
+  end
+
+  def find_room
+    @room = Room.find(params[:id])
   end
 end
