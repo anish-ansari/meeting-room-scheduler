a0abaebbec49e8e3e03507e61f625c95f9d94006
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-12-08 20:45:37 +0545

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "welcome#index"
+  root 'welcome#index'
 
-  resources :rooms
-  resources :bookings
+  resources :rooms do
+    resources :bookings, except: %i[index update destroy edit]
+  end
+  resources :bookings, only: [:index]
 end
