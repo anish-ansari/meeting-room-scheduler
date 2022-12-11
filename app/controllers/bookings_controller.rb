aabc56773bc61d9689793cfbad51fc8c70f02977
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-12-11 17:43:23 +0545

    Add changes to new booking logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,9 @@ class BookingsController < ApplicationController
     @bookings = Booking.where(user_id: current_user.id)
   end
 
-  def show; end
+  def show
+    @booking = Booking.find(params[:id])
+  end
 
   def new
     @room = Room.find(params[:room_id])
@@ -14,36 +16,33 @@ class BookingsController < ApplicationController
 
   def create
     @room = Room.find(params[:room_id])
-    @booking = Booking.new(booking_params)
+    @booking = @room.bookings.new(booking_params)
+    @meeting_date_time = @room.bookings.select('start_date_time, end_date_time')
 
-    @booking.start_date_time = local_to_utc(params[:booking][:start_date_time])
-    @booking.end_date_time = local_to_utc(params[:booking][:end_date_time])
-
-    @all_info = Booking.where(room_id: @room.id).select("start_date_time, end_date_time")
-    arr = []
-    @all_info.each do |val|
-      temp = []
-      temp.push(val[:start_date_time], val[:end_date_time])
-      arr.push(temp)
-      # puts val[:start_date_time]
-      # byebug
-    end
-    puts arr
-    byebug
-
-    if @all_info.empty?
-      if @booking.save
-        redirect_to room_booking_path(@room, @booking)
-      else
-        render "new"
-      end
+    # base case:
+    if @meeting_date_time.empty?
+      @booking.save
+      redirect_to room_booking_path(@room, @booking)
     else
-      if @booking.save
-        redirect_to room_booking_path(@room, @booking)
-      else
-        render "new"
+      flag = true
+      @meeting_date_time.each do |val|
+        if @booking.start_date_time > val.start_date_time && @booking.start_date_time < val.end_date_time
+          flag = false
+          break
+        end
+        if @booking.end_date_time > val.start_date_time && @booking.start_date_time < val.end_date_time
+          flag = false
+          break
+        end
       end
+      if flag == false
+        @flag = false
+        render 'new'
+        return
+      end
+      @booking.save
     end
+    redirect_to room_booking_path(@room, @booking)
   end
 
   private
@@ -53,14 +52,5 @@ class BookingsController < ApplicationController
     params.require(:booking)
           .permit(:description, :start_date_time, :end_date_time)
           .merge(user_id: current_user.id, room_id: @room.id)
-  end
-
-  def find_booking
-    @booking = Booking.find(params[:id])
-  end
-
-  def local_to_utc(time)
-    to_utc = time.split("T")[1].in_time_zone.utc - 5.hours - 45.minutes
-    to_utc.to_s.split(" ").join("T")[0..-8]
   end
 end
